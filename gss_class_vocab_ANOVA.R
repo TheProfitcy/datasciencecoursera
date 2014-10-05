de2d1d7a2d0069f36f3f38419ed53266e7273ded
Jason Eyerly <teamtraders3564@gmail.com>
2014-10-04 18:48:53 -0700

    General Edits

--- gss_class_vocab_ANOVA.R
+++ gss_class_vocab_ANOVA.R
@@ -7,5 +7,5 @@ boxplot(gss$wordsum ~ gss$class)
 
 gss_clean = na.omit(subset(gss, class == "WORKING" | class =="LOWER"))
 
-inference(y = gss_clean$wordsum, x = gss_clean$class, est = "mean", type = "ht", 
-          null = 0, alternative = "twosided", method = "theoretical")
+inference(y = gss_clean$wordsum, x = gss_clean$class, est = "mean", 
+          null = 0, type = "ht", alternative = "twosided", method = "theoretical")
